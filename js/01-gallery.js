a5b1eca1a3331cfa4ffcc5a42212ee6139937315
Ihor7s <sapishchuk.igor@gmail.com>
2023-02-25 09:28:52 +0200

    1dz

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const imageMarkup = createImagesCardsMarkup(galleryItems);
+
+gallery.addEventListener('click', onClickImageCollection);
+
+function createImagesCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+            <div class="gallery__item">
+                <a class="gallery__link href = ${original} onclick="evt.preventDefault()" >
+                    <img
+                        class="gallery__image"
+                        src= "${preview}"
+                        data-source= "${original}"
+                        alt= "${description}"
+                    />
+                </a>
+            </div>`
+            ;
+    }).join('');
+}
+
+gallery.insertAdjacentHTML('beforeend', imageMarkup);
+
+function onClickImageCollection(e) {
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const srcImgOriginal = e.target.dataset.source;
+
+    basicLightbox.create(`
+        <img src = '${srcImgOriginal}'>
+        `).show();
+}
+
+
+
